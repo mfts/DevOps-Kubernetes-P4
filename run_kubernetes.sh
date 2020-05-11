69891dc3a21d0c5ea301c546b7f901deb3b3979d
Marc Seitz <marc@mfts.io>
2020-05-12 01:19:49 +0300

    add kubernetes instructions

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,29 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=mfts/sklearn-flask-housing-prices
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run sklearn-flask-housing-prices\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=sklearn-flask-housing-prices
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
-
-dockerpath="noahgift/flasksklearn"
-
-# Run in Docker Hub container with kubernetes
-kubectl run flaskskearlndemo\
-    --generator=run-pod/v1\
-    --image=$dockerpath\
-    --port=80 --labels app=flaskskearlndemo
-
-# List kubernetes pods
-kubectl get pods
-
-# Forward the container port to host
-kubectl port-forward flaskskearlndemo 8000:80
+kubectl port-forward sklearn-flask-housing-prices 8000:80
